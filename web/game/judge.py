dce66cdca3dca545175fa9fc7c2bf413dde4d358
YichiRockyZhang <rocky1oo.zhang@gmail.com>
2024-04-25 16:30:13 -0400

    Added openai and qa-metrics as dependencies for answer correctness.

--- web/game/judge.py
+++ web/game/judge.py
@@ -9,14 +9,14 @@ model = CloseLLM()
 model.set_openai_api_key(os.getenv("OPENAI_API_KEY"))
 pedant = PEDANT()
 
-def judge_answer_annotation_game(user_answer, question_answer, question, questionPedant):
+def judge_answer_annotation_game(candidate_answer, reference_answer, question, last_clue):
     """Judge answer response as correct - follows QA Metrics answer verification pipeline"""
     correct = False
     try:
-        prompt = f"question: {question}\nreference: {question_answer}\ncandidate: {user_answer}\nIs the candidate answer correct based on the question and reference answer? Please only output 'correct' or 'incorrect'."
+        prompt = f"question: {question}\nreference: {reference_answer}\ncandidate: {candidate_answer}\nIs the candidate answer correct based on the question and reference answer? Please only output 'correct' or 'incorrect'."
         correct = model.prompt_gpt(prompt=prompt, model_engine='gpt-3.5-turbo', temperature=0.1).lower() == "correct"
     except:
-        correct = pedant.evaluate(question_answer, user_answer, questionPedant)
+        correct = pedant.evaluate(reference_answer, candidate_answer, last_clue)
     return correct
 
 def judge_answer_kuiperbowl(user_answer, question_answer):
