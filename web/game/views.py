26bd90c5b8aa5b0b2701cb5e640d9d48852d1302
YichiRockyZhang <rocky1oo.zhang@gmail.com>
2024-04-25 17:10:53 -0400

    New leaderboard metric for answering qb questions.

--- web/game/views.py
+++ web/game/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from django.db.models import Count, Q, F
+from django.db.models import Count, Sum, Case, When, Q, F, FloatField
 
 # Create your views here.
 def home(request):
@@ -26,16 +26,26 @@ def leaderboard(request):
     # Get initial data grouped by player with their guessed_gen_method_correctly counts
     player_stats = (
         QuestionFeedback.objects
-        .values("player__user__id")
-        .annotate(
-            tp=Count('id', filter=Q(guessed_gen_method_correctly=True, guessed_generation_method=Question.GenerationMethod.AI)),
-            fp=Count('id', filter=Q(guessed_gen_method_correctly=False, guessed_generation_method=Question.GenerationMethod.AI)),
-            fn=Count('id', filter=Q(guessed_gen_method_correctly=False, guessed_generation_method=Question.GenerationMethod.HUMAN)),
-            tn=Count('id', filter=Q(guessed_gen_method_correctly=True, guessed_generation_method=Question.GenerationMethod.HUMAN)),
-            total=Count('id'),
-            answered_correctly=Count('id', filter=Q(answered_correctly=True)),
-        )
-        # .filter(total__gt=5)
+    .values("player__user__id")
+    .annotate(
+        tp=Count('id', filter=Q(guessed_gen_method_correctly=True, guessed_generation_method=Question.GenerationMethod.AI)),
+        fp=Count('id', filter=Q(guessed_gen_method_correctly=False, guessed_generation_method=Question.GenerationMethod.AI)),
+        fn=Count('id', filter=Q(guessed_gen_method_correctly=False, guessed_generation_method=Question.GenerationMethod.HUMAN)),
+        tn=Count('id', filter=Q(guessed_gen_method_correctly=True, guessed_generation_method=Question.GenerationMethod.HUMAN)),
+        total=Count('id'),
+        answered_correctly_count=Count('id', filter=Q(answered_correctly=True)),
+        points_scored=Sum(Case(
+            When(answered_correctly=True, then=1.5 - F('buzz_position_norm')),
+            default=0,
+            output_field=FloatField()
+        )),
+        points_against=Sum(Case(
+            When(answered_correctly=False, then=1.5 - F('buzz_position_norm')),
+            default=0,
+            output_field=FloatField()
+        )),
+    )
+        # .filter(total__gt=20)
         # .order_by('-tp')[:100] # may need to uncomment for efficiency
     )
 
@@ -44,8 +54,9 @@ def leaderboard(request):
     for player in player_stats:
         print(player, type(player))
         tp, fp, fn, tn = player['tp'], player['fp'], player['fn'], player['tn']
+        points_scored, points_against = player['points_scored'], player['points_against']
         total = int(tp + fp + fn + tn)
-        answered_correctly = player['answered_correctly']
+        # answered_correctly = player['answered_correctly']
         accuracy = (tp + tn) / float(total) if (total) > 0 else 0
         precision = tp / float(tp + fp) if (tp + fp) > 0 else 0
         recall = tp / float(tp + fn) if (tp + fn) > 0 else 0
@@ -56,16 +67,10 @@ def leaderboard(request):
         print(User.objects.get(id=player['player__user__id']))
         leaderboard_data.append({
             'username': User.objects.get(id=player['player__user__id']).name,
-            'accuracy': accuracy,
-            'precision': precision,
-            'recall': recall,
             'total': total,
+            'expected_wins': 1/(1 + (points_scored / ( points_against + 1e-5) ) ** 1.83),
             'f1': f1,
-            'tp': tp,
-            'fp': fp,
-            'fn': fn,
-            'tn': tn,
-            'question_answered_correctly_rate': answered_correctly / (total) if total > 0 else 0
+            # 'question_answered_correctly_rate': answered_correctly / (total) if total > 0 else 0
         })
 
     # Sort data by F1 score in descending order
