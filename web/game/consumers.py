b01c13b39f58ecc9c666bbabd82c215cc25418ca
nbalepur <nbalepur@umd.edu>
2025-03-10 14:22:43 -0400

    update plan-following

--- web/game/consumers.py
+++ web/game/consumers.py
@@ -660,8 +660,9 @@ class QuizbowlConsumer():
         user = await user_from_player(player)
 
         # (question_id, did_comparison) -> number of users who have done it
+        # right now, do we care if they followed the plan?
         question_user_count = AnswerData.objects.filter(
-                followed_plan=True, is_final=True, is_report=False
+                followed_plan=False, is_final=True, is_report=False
             ).values("question_id", "did_comparison").annotate(user_count=Count("user__user_id", distinct=True))
         question_user_count = await sync_to_async(list)(question_user_count)
         question_to_user_count = {
